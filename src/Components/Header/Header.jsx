ff6001be558ad6230fd0dfc3a5d64714bee930da
Mahliyo <gasandigital13@gmail.com>
2026-07-01 17:43:24 +0500

    darkMode star  style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
         <div className="container">
-            
+            <div className="header-wrapper"></div>
         </div>
     </div>
   )
